2df9a165623359c1dac76e3874e745aee1fbb3c9
Nandhini <nandhinimu27@gmail.com>
2021-11-24 14:00:41 +0000

    added CSS

--- app/javascript/components/Airlines/Airline.js
+++ app/javascript/components/Airlines/Airline.js
@@ -1,17 +1,49 @@
 import React from "react";
+import { BrowserRouter as Router, Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Card = styled.div`
+  border: 3px solid #efefef;
+  background: #fff;
+`;
+const AirlineLogo = styled.div`
+  width: 50px;
+  img {
+    height: 50px;
+    width: 50px;
+    border-radius: 100%;
+    border: 1px solid #efefef;
+  }
+`;
+const AirlineName = styled.div`
+  padding: 20px 0 10px 0;
+`;
+
+const LinkWrapper = styled.div`
+  margin: 30px 0 20px 0;
+  height: 50px;
+  a {
+    color: #fff;
+    background: #000;
+    border-radius: 4px;
+    padding: 10px 50px;
+    border: 1px solid #000;
+    text-decoration: none;
+  }
+`;
 
 const Airline = (props) => {
   return (
-    <div className="card">
-      <div className="airline_logo">
+    <Card>
+      <AirlineLogo>
         <img src={props.attributes.image_url} alt={props.attributes.name} />
-      </div>
-      <div className="airline_name">{props.attributes.name}</div>
+      </AirlineLogo>
+      <AirlineName>{props.attributes.name}</AirlineName>
       <div className="airline_score">{props.attributes.avg_score}</div>
-      <div className="airline_link">
-        <a href={"/airline/${props.attributes.slug}"}>view Airline</a>
-      </div>
-    </div>
+      <LinkWrapper>
+        <Link to={"/airline/${props.attributes.slug}"}>view Airline</Link>
+      </LinkWrapper>
+    </Card>
   );
 };
 export default Airline;
--- app/javascript/components/Airlines/Airlines.js
+++ app/javascript/components/Airlines/Airlines.js
@@ -1,6 +1,32 @@
 import React, { useState, useEffect, Fragment } from "react";
 import axios from "axios";
 import Airline from "./Airline";
+import styled from "styled-components";
+
+const Home = styled.div`
+  text-align: center;
+  max-width: 1200px;
+  margin-left: auto;
+  margin-right: auto;
+`;
+const Header = styled.div`
+  padding: 100px 100px 10px 100px;
+  h1 {
+    font-size: 42px;
+  }
+`;
+const Subheader = styled.div`
+  font-weight: 300;
+  font-size: 42px;
+`;
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  grid-gap: 20px;
+  width: 100%;
+  padding: 20px;
+`;
+
 const Airlines = () => {
   const [airlines, setAirlines] = useState([]);
 
@@ -16,15 +42,13 @@ const Airlines = () => {
   });
 
   return (
-    <div className="home">
-      <div className="header">
+    <Home>
+      <Header>
         <h1>Open flights</h1>
-        <div className="subheader">This is airlines view</div>
-      </div>
-      <div>
-        <ul>{grid}</ul>
-      </div>
-    </div>
+        <Subheader>This is airlines view</Subheader>
+      </Header>
+      <Grid>{grid}</Grid>
+    </Home>
   );
 };
 
